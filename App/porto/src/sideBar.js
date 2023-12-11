8745447142fca61d81b8db4235dda7e4afd12963
mohamed14787 <mohamedelshaarawy87@gmail.com>
2023-12-11 21:27:43 +0200

    .

--- App/porto/src/sideBar.js
+++ App/porto/src/sideBar.js
@@ -4,6 +4,11 @@ import { PiCertificateBold } from "react-icons/pi";
 import { CiLinkedin } from "react-icons/ci";
 import { FaGithub } from "react-icons/fa";
 import { SiGmail } from "react-icons/si";
+import { SiLeetcode } from "react-icons/si";
+
+
+
+
 
 
 
@@ -13,11 +18,14 @@ function SideBar() {
     <div className="sideBar">
        
         <PiCertificateBold   style={{color:"white",paddingTop:"4em",width:"35px",height:"35px"}} />
+        <SiGmail  style={{color:"white",paddingTop:"2em",width:"25px",height:"35px",paddingLeft:"0.4em"}} />
         <CiLinkedin style={{color:"white",paddingTop:"2em",width:"35px",height:"35px"}}  />
         <FaGithub style={{color:"white",paddingTop:"2em",width:"35px",height:"35px"}} />
+        <SiLeetcode style={{color:"white",paddingTop:"2em",width:"35px",height:"35px"}}/>
+
         <MdOutlineVolunteerActivism  style={{color:"white",paddingTop:"2em",width:"35px",height:"35px"}}/>
 
-        <SiGmail  style={{color:"white",paddingTop:"2em",width:"25px",height:"35px",paddingLeft:"0.4em"}} />
+       
 
 
 
